0378f2db131e54fe65818ca95c73c2934ce5b6c5
CanhNguyen <leagueyasuo186@gmail.com>
2022-09-14 14:52:20 +0700

    k

---
+++ src/main/java/com/example/tttutorials/TtTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tttutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tttutorials/TtTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tttutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
